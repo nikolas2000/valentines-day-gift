fa23394c8286f1a355f92aca9f962f2a58c0e247
Nikolaos Kyriakakis <47464669+nikolas2000@users.noreply.github.com>
2025-01-06 00:43:53 +0200

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,55 @@
+# I created a function that prints a heart using only symbols and spaces. Just for fun!
+# 5/1/2024.
+
+import time
+symbol ="*" #-,+,*,e.t.c.
+
+"""
+Calls the heart function and sets the user input
+"""
+def sendHeart():
+    width = input() #19 for example
+    validation = int(width)%2!=0 and int(width)>=7 and int(width)<=45
+    if(validation == False):
+        print("Please give a correct odd number (>=7 & <=45)")
+        sendHeart()
+    else:
+        heart(int(width))
+
+"""
+Prints both top and bottom heart's parts
+"""    
+def heart(width):
+    upBody(width)
+    downBody(width)
+
+"""
+Prints heart's top part
+"""    
+def upBody(length):
+    leftSpaces=3
+    midSpaces=5
+    maxleftStars = (length-2)//2 #-2 because we the full symbol line is included in the down body
+    decreaseLeftSpaces=4
+    while (leftSpaces>0):
+        leftStars = maxleftStars - decreaseLeftSpaces
+        print(" " * leftSpaces + symbol * leftStars + " " * midSpaces + symbol * leftStars)
+        time.sleep(0.5) #put a delay for 0.5 second just for the simple "animation"
+        leftSpaces-=1
+        midSpaces-=2
+        decreaseLeftSpaces-=2
+
+"""
+Prints the bottom part, which looks like an upside down tree
+"""   
+def downBody(length):
+    stars=length
+    leftSpaces=0
+    while stars>0:
+        print(" " * leftSpaces + symbol * stars)
+        time.sleep(0.5) #put a delay for 0.5 second just for the  simple "animation"
+        stars-=2
+        leftSpaces+=1
+
+print("Send love by giving a correct odd number (>=7 & <=45)")
+sendHeart()
